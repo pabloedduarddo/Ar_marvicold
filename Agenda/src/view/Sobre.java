a92a7de57012d19837fd8d0134afa8e7d25d5293
Pablo Eduardo Moreira <95319664+pabloedduarddo@users.noreply.github.com>
2023-08-03 19:08:01 -0300

    Add files via upload

---
+++ Agenda/src/view/Sobre.java
@@ -0,0 +1,60 @@
+package view;
+
+import java.awt.EventQueue;
+
+import javax.swing.JDialog;
+import java.awt.Toolkit;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.ImageIcon;
+
+public class Sobre extends JDialog {
+
+	private static final long serialVersionUID = 1L;
+
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Sobre dialog = new Sobre();
+					dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+					dialog.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	public Sobre() {
+		setModal(true);
+		setResizable(false);
+		setTitle("Sobre");
+		setIconImage(Toolkit.getDefaultToolkit().getImage(Sobre.class.getResource("/img/notepad.png")));
+		setBounds(100, 100, 450, 300);
+		getContentPane().setLayout(null);
+
+		JLabel lblNewLabel = new JLabel("Agenda de Contatos");
+		lblNewLabel.setFont(new Font("Times New Roman", Font.BOLD, 14));
+		lblNewLabel.setBounds(30, 22, 204, 14);
+		getContentPane().add(lblNewLabel);
+
+		JLabel lblNewLabel_1 = new JLabel("Autor: Pablo Eduardo");
+		lblNewLabel_1.setBounds(30, 60, 142, 14);
+		getContentPane().add(lblNewLabel_1);
+
+		JLabel lblNewLabel_2 = new JLabel("Versão 1.0");
+		lblNewLabel_2.setBounds(40, 95, 63, 14);
+		getContentPane().add(lblNewLabel_2);
+
+		JLabel lblNewLabel_3 = new JLabel("Sob a licença MIT");
+		lblNewLabel_3.setBounds(40, 180, 119, 14);
+		getContentPane().add(lblNewLabel_3);
+
+		JLabel lblNewLabel_4 = new JLabel("");
+		lblNewLabel_4.setIcon(new ImageIcon(Sobre.class.getResource("/img/mit-icon.png")));
+		lblNewLabel_4.setBounds(285, 105, 128, 128);
+		getContentPane().add(lblNewLabel_4);
+
+	}
+}
